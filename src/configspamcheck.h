7a8768667ce1fedb37382bcbc1b21993a80fb10d
kuddel-fl <kuddel-fl@9bd4a48d-2b12-44a0-a1bc-49c5f8e4eea2>
2010-03-23 20:37:58 +0000

    license added
    git-svn-id: https://svn.code.sf.net/p/kshowmail/svn/trunk@142 9bd4a48d-2b12-44a0-a1bc-49c5f8e4eea2

--- src/configspamcheck.h
+++ src/configspamcheck.h
@@ -1,14 +1,20 @@
-//
-// C++ Interface: configspamcheck
-//
-// Description:
-//
-//
-// Author: Ulrich Weigelt <ulrich.weigelt@gmx.de>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/*
+Copyright 2010  Ulrich Weigelt <ulrich.weigelt@gmx.de>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of
+the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef CONFIGSPAMCHECK_H
 #define CONFIGSPAMCHECK_H
 
